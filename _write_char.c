38237d7cea62aa9a3036d40f5fcdd2d226407bc8
charlespaul1 <charlespaulwabomba@gmail.com>
2022-08-09 12:35:53 -0700

    writef

--- _write_char.c
+++ _write_char.c
@@ -5,7 +5,7 @@
  * @c: The character to print
  *
  * Return: On success return 1
- * On error, -1 is returned, and errno is set appropriately.
+ * On error, -1 is returned, and errno is set appropriately
  */
 int _write_char(char c)
 {
